72a074fca30f86a843890af760b06c0eea89eb04
Sean Patton <seanpatton1@outlook.com>
2024-09-16 19:15:50 +0000

    enable serving of image files

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-seanpatton1-djangoblog-w7naqoljjjc.ws.codeinstitute-ide.net', ".herokuapp.com"]
 
@@ -43,12 +43,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
     'crispy_forms',
     'crispy_bootstrap5',
+    'cloudinary',
     "blog",
     "django_summernote",
     "about",
